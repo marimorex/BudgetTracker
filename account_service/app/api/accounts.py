959863f69ed89876b4652cf5f4e8c970b657f343
Maria Gabriela Moreno Exposito <marimorexx@gmail.com>
2024-10-18 13:06:17 +0200

    feat(account): run pre-commit

--- account_service/app/api/accounts.py
+++ account_service/app/api/accounts.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # Endpoint to list all accounts
-@app.get("/accounts/",)
+@app.get(
+    "/accounts/",
+)
 def list_accounts():
     return "hello world"
