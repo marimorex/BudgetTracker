fe0629f28011bd065162c2cafce8ddd14f5f1b32
Maria Gabriela Moreno Exposito <marimorexx@gmail.com>
2025-01-07 14:30:23 +0100

    restructure : extracted generic files and updated dockerfile

--- helpers/logger.py
+++ helpers/logger.py
@@ -1,4 +1,5 @@
 from helpers.logger_generic import setup_logger, request_id_context
+import os
 
 
 class Logger:
@@ -6,9 +7,7 @@ class Logger:
 
     def __new__(self):
         if self._instance is None:
-            self._instance = setup_logger(
-                "account_service"
-            )  # todo use later ENV variables
+            self._instance = setup_logger(os.getenv("SERVICE_NAME"))
         return self._instance
 
 
